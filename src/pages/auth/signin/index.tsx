72621a70923b3eb526dcc320eb187ceec5b1100a
bettaibi <bettaibinidhal00@gmail.com>
2021-03-24 11:43:00 +0100

    auth pages have been created

--- src/pages/auth/signin/index.tsx
+++ src/pages/auth/signin/index.tsx
@@ -1,9 +1,93 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Card, Form, Container, Button } from 'react-bootstrap';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+
+import logo from '../../../assets/img/logo90.png';
+
+interface LoginForm{
+    email: string;
+    password: string;
+}
+
+const defaultState: LoginForm = {email: '', password: ''};
+
+const schema = yup.object().shape({
+    email: yup.string().required('Email Is required').email('Invalid Email'),
+    password: yup.string().required('Password is required').min(6, "Password's length should be greater than 6")
+    .max(20, 'too much letters')
+});
 
 export const Signin: React.FC = () => {
+    const history = useHistory();
+
+    const navigateTo = (e: any) =>{
+        e.preventDefault();
+        history.push('/signup')
+    }
+
     return (
-        <div>
-            this is a login page
-        </div>
+        <Container className="d-flex flex-column justify-content-center align-items-center vh-100">
+
+            <Card style={{minWidth: '300px'}}>
+                <Card.Header className="text-center">
+                    <img src={logo} alt="App Logo" />
+                    <h6 className="fw-400">SIGN IN TO YOUR ACCOUNT</h6>
+                </Card.Header>
+                <Card.Body>
+                    <Formik initialValues={defaultState} validationSchema={schema} onSubmit={(values)=>console.log(values)}>
+                        {
+                            ({handleSubmit, handleChange, handleBlur, values, touched, errors, isValid}) => (
+                                <Form onSubmit = {handleSubmit}>
+                                <Form.Group>
+                                    <Form.Label>Enter your email</Form.Label>
+                                    <Form.Control placeholder="email" autoComplete="off" name="email"
+                                    onChange={handleChange} onBlur={handleBlur} value={values.email}
+                                    isValid={touched.email && !errors.email}
+                                    isInvalid={touched.email && !!errors.email}></Form.Control>
+                                  { !errors.email && <Form.Text className="text-muted">
+                                        We'll never share your email with anyone else.
+                                    </Form.Text>}
+                                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="invalid">
+                                        {errors.email}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Enter your password</Form.Label>
+                                    <Form.Control placeholder="******" type="password" autoComplete="off" name="password"
+                                     onChange={handleChange} onBlur={handleBlur} value={values.password}
+                                     isValid={touched.password && !errors.password}
+                                     isInvalid={touched.password && !!errors.password}></Form.Control>
+                                    <a href="!#">
+                                        <small>Recover password</small>
+                                    </a>
+                                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="invalid">
+                                        {errors.password}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <div className="text-right">
+                                    <Button type="submit" variant="primary mr-2" disabled={!isValid}>Login</Button>
+                                    <Button variant="outline-primary" onClick={navigateTo}>Create an account!</Button>
+                                </div>
+                            </Form>
+                            )
+                        }
+                    </Formik>
+
+                    <div className="text-center p-3">
+                        <span className="or__separator text-secondary">
+                         OR
+                        </span>
+                    </div>
+
+                    <div className="text-center">
+                        <Button variant="danger">Login with Google</Button>
+                    </div>
+                </Card.Body>
+            </Card>
+        </Container>
     )
 }
--- src/pages/auth/signup/index.tsx
+++ src/pages/auth/signup/index.tsx
@@ -1,9 +1,95 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { Card, Form, Container, Button } from 'react-bootstrap';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+
+import logo from '../../../assets/img/logo90.png';
+
+interface LoginForm{
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+const defaultState: LoginForm = {email: '', password: '', confirmPassword: ''};
+    
+const schema = yup.object().shape({
+    email: yup.string().required('Email Is required').email('Invalid Email'),
+    password: yup.string().required('Password is required').min(6, "Password's length should be greater than 6")
+    .max(20, 'too much letters'),
+    confirmPassword: yup.string().required('Password confirmation is required').min(6, "Password's length should be greater than 6")
+    .max(20, 'too much letters').oneOf([yup.ref('password')], 'Password must much')
+});
 
 export const Singup: React.FC = () => {
+    const history = useHistory();
+
+    const navigateTo = (e: any) =>{
+        e.preventDefault();
+        history.push('/signin')
+    }
+
     return (
-        <div>
-            SingnUp component
-        </div>
+        <Container className="d-flex flex-column justify-content-center align-items-center vh-100">
+
+            <Card style={{minWidth: '300px'}}>
+                <Card.Header className="text-center">
+                    <img src={logo} alt="App Logo" />
+                    <h6 className="fw-400">SIGN IN TO YOUR ACCOUNT</h6>
+                </Card.Header>
+                <Card.Body>
+                    <Formik initialValues={defaultState} validationSchema={schema} onSubmit={(values)=>console.log(values)}>
+                        {
+                            ({handleSubmit, handleChange, handleBlur, values, touched, errors, isValid}) => (
+                                <Form onSubmit = {handleSubmit}>
+                                <Form.Group>
+                                    <Form.Label>Enter your email</Form.Label>
+                                    <Form.Control placeholder="email" autoComplete="off" name="email"
+                                    onChange={handleChange} onBlur={handleBlur} value={values.email}
+                                    isValid={touched.email && !errors.email}
+                                    isInvalid={touched.email && !!errors.email}></Form.Control>
+                                  { !errors.email && <Form.Text className="text-muted">
+                                        We'll never share your email with anyone else.
+                                    </Form.Text>}
+                                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="invalid">
+                                        {errors.email}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Enter your password</Form.Label>
+                                    <Form.Control placeholder="******" type="password" autoComplete="off" name="password"
+                                     onChange={handleChange} onBlur={handleBlur} value={values.password}
+                                     isValid={touched.password && !errors.password}
+                                     isInvalid={touched.password && !!errors.password}></Form.Control>
+                                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="invalid">
+                                        {errors.password}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label>Confirm your password</Form.Label>
+                                    <Form.Control placeholder="******" type="password" autoComplete="off" name="confirmPassword"
+                                     onChange={handleChange} onBlur={handleBlur} value={values.confirmPassword}
+                                     isValid={touched.confirmPassword && !errors.confirmPassword}
+                                     isInvalid={touched.confirmPassword && !!errors.confirmPassword}></Form.Control>
+                                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="invalid">
+                                        {errors.confirmPassword}
+                                    </Form.Control.Feedback>
+                                </Form.Group>
+                                <div className="text-right">
+                                    <Button type="submit" variant="primary mr-2" disabled={!isValid}>Create</Button>
+                                    <Button variant="outline-primary" onClick={navigateTo}>Login!</Button>
+                                </div>
+                            </Form>
+                            )
+                        }
+                    </Formik>
+                </Card.Body>
+            </Card>
+        </Container>
     )
+  
 }
